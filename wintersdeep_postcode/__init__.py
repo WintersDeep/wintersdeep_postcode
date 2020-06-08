946cd47a6f0fc157378448ac973ea7e415014986
glim <admin@wintersdeep.com>
2020-06-08 08:31:29 +0000

    ADDED: some meta data to the module __init__.py

---
+++ wintersdeep_postcode/__init__.py
@@ -0,0 +1,10 @@
+__author__      =    "Adam Williams"
+__copyright__   =    "Copyright 2020, WintersDeep.com"
+
+__license__     =    "MIT"
+__version__     =    "0.0.0a1"
+__credits__     =    [ ]
+
+__maintainer__  =   "WintersDeep.com"
+__email__       =   "admin@wintersdeep.com"
+__status__      =   "Development"
