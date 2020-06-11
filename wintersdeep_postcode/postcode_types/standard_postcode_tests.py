4b984ffcfa40548cbdd02e99a9e0fd24f511e6e8
glim <admin@wintersdeep.com>
2020-06-11 09:54:53 +0000

    MODIFIED: cleaned up the tests fot the standard postcode a little bit.

--- wintersdeep_postcode/postcode_types/standard_postcode_tests.py
+++ wintersdeep_postcode/postcode_types/standard_postcode_tests.py
@@ -19,6 +19,20 @@ from wintersdeep_postcode.postcode_types.standard_postcode import StandardPostco
 ## Unit Test class for the StandardPostcode class
 class TestStandardPostcode(TestCase):
 
+    ## Sets up static memebers that are reused over tests.
+    #  @param cls the class that is invoking this method.
+    @classmethod
+    def setUpClass(cls):
+        cls.NormalRegex = StandardPostcode.GetParseRegex(r"\ ")
+
+    ## Creates a standard postcode from an string.
+    #  @param cls the class that is invoking this method.
+    #  @param postcode_string the postcode string to convert.
+    def createStandardPostcode(cls, postcode_string):
+        regex_match = cls.NormalRegex.match(postcode_string)
+        result_postcode = StandardPostcode(regex_match)
+        return result_postcode
+
     ## tests that the function responsible for creating the parser regex, respects the
     #  whitespace delimiter that is passed to it.
     def test__StandardPostcode_GetParseRegex__delimiter(self):
@@ -34,129 +48,74 @@ class TestStandardPostcode(TestCase):
     ## tests that, at a glance, the regular expression being returned by the factory
     #  appears to able to successfully parse good values, and reject bad ones.
     def test__StandardPostcode_GetParseRegex__regex(self):
-        test_regex = StandardPostcode.GetParseRegex(r"\ ")    
-        self.assertIsNotNone( test_regex.match("A1 1AA") )
-        self.assertIsNotNone( test_regex.match("A11 1AA") )
-        self.assertIsNotNone( test_regex.match("A1A 1AA") )
-        self.assertIsNotNone( test_regex.match("AA1 1AA") )
-        self.assertIsNotNone( test_regex.match("AA11 1AA") )
-        self.assertIsNotNone( test_regex.match("AA1A 1AA") )
+        
+        test_regex = StandardPostcode.GetParseRegex(r"\ ")
+
+        test_list = [
+            "A1 1AA",
+            "A11 1AA",
+            "A1A 1AA",
+            "AA1 1AA",
+            "AA11 1AA",
+            "AA1A 1AA"
+        ]
+
+        for test_string in test_list:
+            self.assertIsNotNone( test_regex.match(test_string) )
 
     ## tests that the standard postcode object allocates local members as expected.
-    def test__StadardPostcode_ctor(self):
-    
-        test_regex = StandardPostcode.GetParseRegex(r"\ ")    
-    
-        regex_match = test_regex.match("A1 2BC")
-        test_postcode = StandardPostcode(regex_match)
-        self.assertEqual(test_postcode.outward_area, "A")
-        self.assertEqual(test_postcode.outward_district, 1)
-        self.assertEqual(test_postcode.outward_subdistrict, "")
-        self.assertEqual(test_postcode.inward_sector, 2)
-        self.assertEqual(test_postcode.inward_unit, "BC")
+    def test__StandardPostcode_ctor(self):
 
-        regex_match = test_regex.match("A12 3BC")
-        test_postcode = StandardPostcode(regex_match)
-        self.assertEqual(test_postcode.outward_area, "A")
-        self.assertEqual(test_postcode.outward_district, 12)
-        self.assertEqual(test_postcode.outward_subdistrict, "")
-        self.assertEqual(test_postcode.inward_sector, 3)
-        self.assertEqual(test_postcode.inward_unit, "BC")
+        test_list = [
+            (r"A1 2BC",     "A",    1,  "",     2, "BC"),
+            (r"A12 3BC",    "A",    12, "",     3, "BC"),
+            (r"A1B 2CD",    "A",    1,  "B",    2, "CD"),
+            (r"AB1 2CD",    "AB",   1,  "",     2, "CD"),
+            (r"AB12 3CD",   "AB",   12, "",     3, "CD"),
+            (r"AB1C 2DE",   "AB",   1,  "C",    2, "DE")
+        ]
         
-        regex_match = test_regex.match("A1B 2CD")
-        test_postcode = StandardPostcode(regex_match)
-        self.assertEqual(test_postcode.outward_area, "A")
-        self.assertEqual(test_postcode.outward_district, 1)
-        self.assertEqual(test_postcode.outward_subdistrict, "B")
-        self.assertEqual(test_postcode.inward_sector, 2)
-        self.assertEqual(test_postcode.inward_unit, "CD")
-        
-        regex_match = test_regex.match("AB1 2CD")
-        test_postcode = StandardPostcode(regex_match)
-        self.assertEqual(test_postcode.outward_area, "AB")
-        self.assertEqual(test_postcode.outward_district, 1)
-        self.assertEqual(test_postcode.outward_subdistrict, "")
-        self.assertEqual(test_postcode.inward_sector, 2)
-        self.assertEqual(test_postcode.inward_unit, "CD")
-        
-        regex_match = test_regex.match("AB12 3CD")
-        test_postcode = StandardPostcode(regex_match)
-        self.assertEqual(test_postcode.outward_area, "AB")
-        self.assertEqual(test_postcode.outward_district, 12)
-        self.assertEqual(test_postcode.outward_subdistrict, "")
-        self.assertEqual(test_postcode.inward_sector, 3)
-        self.assertEqual(test_postcode.inward_unit, "CD")
-        
-        regex_match = test_regex.match("AB1C 2DE")
-        test_postcode = StandardPostcode(regex_match)
-        self.assertEqual(test_postcode.outward_area, "AB")
-        self.assertEqual(test_postcode.outward_district, 1)
-        self.assertEqual(test_postcode.outward_subdistrict, "C")
-        self.assertEqual(test_postcode.inward_sector, 2)
-        self.assertEqual(test_postcode.inward_unit, "DE")
-
-        # final check for some general properties
-        self.assertEqual(test_postcode.postcode_type, "standard")
-        self.assertFalse(test_postcode.is_validated)
-        self.assertIs(test_postcode._original_regex_match, regex_match)
+        for postcode, area, district, subdistrict, sector, unit in test_list:
+            test_postcode = self.createStandardPostcode(postcode)
+            self.assertEqual(test_postcode.outward_area, area)
+            self.assertEqual(test_postcode.outward_district, district)
+            self.assertEqual(test_postcode.outward_subdistrict, subdistrict)
+            self.assertEqual(test_postcode.inward_sector, sector)
+            self.assertEqual(test_postcode.inward_unit, unit)
+            self.assertEqual(test_postcode.postcode_type, "standard")
+            self.assertFalse(test_postcode.is_validated)
 
     ## Test that the standard postcodes outward code property words as expected.
     def test__StandardPostcode_outward_code(self):
 
-        test_regex = StandardPostcode.GetParseRegex(r"\ ")    
-     
-        regex_match = test_regex.match("AB1C 2DE")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.outward_code, "AB1C")
-        
-        regex_match = test_regex.match("AB12 3CD")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.outward_code, "AB12")
-        
-        regex_match = test_regex.match("AB1 2CD")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.outward_code, "AB1")
-        
-        regex_match = test_regex.match("A1B 2CD")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.outward_code, "A1B")
-        
-        regex_match = test_regex.match("A12 3BC")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.outward_code, "A12")
-        
-        regex_match = test_regex.match("A1 2BC")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.outward_code, "A1")
+        test_list = [
+            ("AB1C 2DE", "AB1C"),
+            ("AB12 3CD", "AB12"),
+            ("AB1 2CD", "AB1"),
+            ("A1B 2CD", "A1B"),
+            ("A12 3BC", "A12"),
+            ("A1 2BC", "A1")
+        ]
+
+        for postcode, outward_code in test_list:
+            postcode = self.createStandardPostcode(postcode)
+            self.assertEqual(postcode.outward_code, outward_code)
 
     ## Test that the standard postcodes inward code property words as expected.
     def test__StandardPostcode_inward_code(self):
 
-        test_regex = StandardPostcode.GetParseRegex(r"\ ")    
-     
-        regex_match = test_regex.match("AB1C 2DE")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.inward_code, "2DE")
-        
-        regex_match = test_regex.match("AB12 3CD")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.inward_code, "3CD")
-        
-        regex_match = test_regex.match("AB1 2CD")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.inward_code, "2CD")
-        
-        regex_match = test_regex.match("A1B 2CD")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.inward_code, "2CD")
-        
-        regex_match = test_regex.match("A12 3BC")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.inward_code, "3BC")
-        
-        regex_match = test_regex.match("A1 2BC")
-        postcode = StandardPostcode(regex_match)
-        self.assertEqual(postcode.inward_code, "2BC")
+        test_list = [
+            ("AB1C 2DE", "2DE"),
+            ("AB12 3CD", "3CD"),
+            ("AB1 2CD", "2CD"),
+            ("A1B 2CD", "2CD"),
+            ("A12 3BC", "3BC"),
+            ("A1 2BC", "2BC")
+        ]
+
+        for postcode, inward_code in test_list:
+            postcode = self.createStandardPostcode(postcode)
+            self.assertEqual(postcode.inward_code, inward_code)
 
     ## Tests the the standard postcode object __str__ method works as expected.
     def test__StandardPostcode_str(self):
@@ -165,43 +124,44 @@ class TestStandardPostcode(TestCase):
             "AB1C 2DE", "AB12 3CD", "AB1 2CD", "A1B 2CD", "A12 3CD", "A1 2BC"
         ]
 
-        test_regex = StandardPostcode.GetParseRegex(r"\ ")    
-        
         for test_string in test_candidates:
-            regex_match = test_regex.match(test_string)
-            postcode = StandardPostcode(regex_match)
+            postcode = self.createStandardPostcode(test_string)
             self.assertEqual( str(postcode), test_string)
 
     ## Tests that fault numbers haven't changed as this would constitute 
     #  breaking changes.
     def test__StandardPostcode__fault_numbers(self):
-        self.assertEqual( int(StandardPostcode.ExpectedSingleDigitDistrict), 201 )
-        self.assertEqual( int(StandardPostcode.ExpectedDoubleDigitDistrict), 202 )
-        self.assertEqual( int(StandardPostcode.NoZeroDistrict), 203 )
-        self.assertEqual( int(StandardPostcode.NoTenDistrict), 204 )
-        self.assertEqual( int(StandardPostcode.SubdistrictsUnsupported), 205 )
-        self.assertEqual( int(StandardPostcode.UnexpectedDistrictSubdivision), 206 )
-        self.assertEqual( int(StandardPostcode.UnusedCharacterInFirstPosition), 207)
+
+        test_list = [
+            ( StandardPostcode.ExpectedSingleDigitDistrict, 201 ),
+            ( StandardPostcode.ExpectedDoubleDigitDistrict, 202 ),
+            ( StandardPostcode.NoZeroDistrict, 203 ),
+            ( StandardPostcode.NoTenDistrict, 204 ),
+            ( StandardPostcode.SubdistrictsUnsupported, 205 ),
+            ( StandardPostcode.UnexpectedDistrictSubdivision, 206 ),
+            ( StandardPostcode.UnusedCharacterInFirstPosition, 207 )
+        ]
+
+        for error_object, expected_id in test_list:
+            self.assertEqual(int(error_object), expected_id)
 
     ## Checks that the method we use to bind tests and faults is working
     def test__StandardPostcode_Validate__fault_assignment(self):
     
-        test_regex = StandardPostcode.GetParseRegex(r"\ ")    
-        
-        # we'll just trigger a fault and make sure an error is assigned to it
-        def check_for_fault(postcode, fault):
-            regex_match = test_regex.match(postcode)
-            postcode = StandardPostcode(regex_match)
+        test_list = [
+            ("BR10 2XX", StandardPostcode.ExpectedSingleDigitDistrict),
+            ("LL9 2XX", StandardPostcode.ExpectedDoubleDigitDistrict),
+            ("LL0 2XX", StandardPostcode.NoZeroDistrict),
+            ("BL10 2XX", StandardPostcode.NoTenDistrict),       
+            ("XY7N 2XX", StandardPostcode.SubdistrictsUnsupported),         
+            ("N1S 2XX", StandardPostcode.UnexpectedDistrictSubdivision),        
+            ("X1 2XX", StandardPostcode.UnusedCharacterInFirstPosition)
+        ]
+
+        for test_string, expected_fault in test_list:       
+            postcode = self.createStandardPostcode(test_string)
             faults = StandardPostcode.Validate(postcode)
-            self.assertTrue(fault in faults)
-        
-        check_for_fault("BR10 2XX", StandardPostcode.ExpectedSingleDigitDistrict)
-        check_for_fault("LL9 2XX", StandardPostcode.ExpectedDoubleDigitDistrict)
-        check_for_fault("LL0 2XX", StandardPostcode.NoZeroDistrict)
-        check_for_fault("BL10 2XX", StandardPostcode.NoTenDistrict)            
-        check_for_fault("XY7N 2XX", StandardPostcode.SubdistrictsUnsupported)            
-        check_for_fault("N1S 2XX", StandardPostcode.UnexpectedDistrictSubdivision)            
-        check_for_fault("X1 2XX", StandardPostcode.UnusedCharacterInFirstPosition)            
+            self.assertTrue(expected_fault in faults)
 
 
 
