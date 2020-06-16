87ecd0db7a94fd41dbed60163adde45362806146
glim <admin@wintersdeep.com>
2020-06-16 00:28:35 +0000

    MODIFIED: pulled the PostcodeError into the module level scope for ease of access.

--- wintersdeep_postcode/__init__.py
+++ wintersdeep_postcode/__init__.py
@@ -12,7 +12,7 @@ __status__      =   "Development"
 
 # Import the most relevant classes up to the module scope.
 from wintersdeep_postcode.postcode_parser import PostcodeParser
-from wintersdeep_postcode.exceptions import (ValidationError, ParseError)
+from wintersdeep_postcode.exceptions import (PostcodeError, ValidationError, ParseError)
 
 ## Parses a postcode string using the default postcode parser.
 #  @param postcode_string the postcode string that should be parsed.
@@ -26,12 +26,12 @@ def parse_postcode(postcode_string):
 ## Parses a postcode using the default postcode parser, in a more forgiving manner.
 #  @param postcode_string the postcode string that should be parsed.
 #  @param default_value the value to return in the event that the postcode cannot be parsed.
-#  @param ignore_validation_error will still return a postcode object, even if it doesn't validate.
+#  @param ignore_validation_errors will still return a postcode object, even if it doesn't validate.
 #  @returns the postcode object on success, or the default value on failure.
-def try_parse_postcode(postcode_string, default_value=None, ignore_validation_error=False):
+def try_parse_postcode(postcode_string, default_value=None, ignore_validation_errors=False):
     try:
         return parse_postcode(postcode_string)
     except ValidationError as ex:
-        return ex.postcode if ignore_validation_error else default_value
+        return ex.postcode if ignore_validation_errors else default_value
     except ParseError as ex:
         return default_value
